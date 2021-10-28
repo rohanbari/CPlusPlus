89c593cd6aaa92b80f365f0cb321596530fee3f5
rohanbari <rohanbari4@gmail.com>
2021-10-28 11:22:36 +0530

    helloWorld.cpp creation
     * Added the repository image in README.md
     * Updated LICENSE
     * VS Code configuration added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# CPlusPlus
-Having some fun with the C++ programming language
+[![cplusplus.png](https://i.postimg.cc/T3ShXPbY/cplusplus.png)](https://postimg.cc/TKVTVfQZ)
+
+Having some fun with the C++ programming language.
---
+++ src/helloWorld.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file helloWorld.cpp
+ * @author Rohan Bari (rohanbari4@gmail.com)
+ * @brief Saying hello to C++ programming language, once again.
+ * @version 0.1
+ * @date 2021-10-28
+ * 
+ * @copyright Copyright (c) 2021 Rohan Bari
+ * 
+ */
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello world from C++!\n";
+    return EXIT_SUCCESS;
+}
