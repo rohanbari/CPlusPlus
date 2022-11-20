4c1bb627423a76dcb9bed541443b3e4f744cf2fd
rohanbari <rohanbari4@gmail.com>
2022-11-20 19:55:01 +0530

    cinttypes with unsigned modifier

--- src/numbers/SpecificIntegers.cpp
+++ src/numbers/SpecificIntegers.cpp
@@ -27,10 +27,23 @@ int main(void)
     int32_t normalInteger = numeric_limits<int32_t>::max();
     int64_t longlongInteger = numeric_limits<int64_t>::max();
 
-    cout << static_cast<int>(signedChar) << endl
-         << shortInteger << endl
-         << normalInteger << endl
-         << longlongInteger << endl;
+    cout << "Signed types:" << endl;
+    cout << "signed char: " << static_cast<int16_t>(signedChar) << endl
+         << "signed short: " << shortInteger << endl
+         << "signed int: " << normalInteger << endl
+         << "signed long long: " << longlongInteger << endl
+         << endl;
+
+    cout << "Unsigned types:" << endl;
+    uint8_t unsignedChar = numeric_limits<uint8_t>::max();
+    uint16_t unsignedShort = numeric_limits<uint16_t>::max();
+    uint32_t unsignedInteger = numeric_limits<uint32_t>::max();
+    uint64_t unsignedLongLongInteger = numeric_limits<uint64_t>::max();
+
+    cout << "unsigned char: " << static_cast<uint16_t>(unsignedChar) << endl
+         << "unsigned short: " << unsignedShort << endl
+         << "unsigned int: " << unsignedInteger << endl
+         << "unsigned long long: " << unsignedLongLongInteger << endl;
 
     return EXIT_SUCCESS;
 }
