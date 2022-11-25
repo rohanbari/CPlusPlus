c6a7cce32407d68787abe27e2229bcd675afd38d
rohanbari <rohanbari4@gmail.com>
2022-11-25 19:04:58 +0530

    Extension of OldSchool.cpp

--- src/containers/OldSchool.cpp
+++ src/containers/OldSchool.cpp
@@ -19,8 +19,15 @@ int main(void)
 {
     int array[SIZE] = { 10, 2, 6, 3, 9 };
 
+    // For-loop method
     for (int i = 0; i < SIZE; i++)
         cout << array[i] << ' ';
+    cout << endl;
+
+    // Range-based for-loop method.
+    // Won't work when array-to-pointer decay takes place.
+    for (const auto& it : array)
+        cout << it << ' ';
     cout << endl;
 
     return EXIT_SUCCESS;
