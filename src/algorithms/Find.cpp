ae084fbf3d7ceded7cf331806f6e0f0bb71d22c9
rohanbari <rohanbari4@gmail.com>
2022-12-02 21:17:58 +0530

    std::find implementation

---
+++ src/algorithms/Find.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file Find.cpp
+ * @author rohanbari (rohanbari4@gmail.com)
+ * @brief The STL provides the find function to retrieve iterators to the first
+ * element in a sequence that matches a supplied value. Let's use 'em!
+ *
+ * @version 0.1
+ * @date 2022-12-02
+ *
+ * Copyright (c) 2022 rohanbari
+ *
+ */
+
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    // A sample string -- let me appreciate myself.
+    string strGreat { "Rohan is one of the best programmers in the world." };
+    auto countT = find(strGreat.cbegin(), strGreat.cend(), 't');
+
+    // Iteration keep continues until the end.
+    while (countT != strGreat.cend()) {
+        // Display when theCount is filled.
+        cout << *countT << endl;
+        // ++theCount changes the iterator to the next iterator element.
+        countT = find(++countT, strGreat.cend(), 't');
+    }
+
+    // std::find_if, std::find_if_not, etc. can be used with a user-defined
+    // predicate (comparator.)
+
+    return EXIT_SUCCESS;
+}
