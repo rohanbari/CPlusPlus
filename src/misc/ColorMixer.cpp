b2a0f7d2c39eb82c8bd7ad4f9a393f7686f590dd
Rohan Bari <rohanbari4@gmail.com>
2023-01-20 20:17:11 +0530

    ColorMixer.cpp: Implemented converter
    - Still needs to fix up the new color algorithm

--- src/misc/ColorMixer.cpp
+++ src/misc/ColorMixer.cpp
@@ -18,30 +18,105 @@
 
 using Colors = std::vector<std::string>;
 
+struct RGB {
+    double R;
+    double G;
+    double B;
+
+    friend RGB operator+=(const RGB& lhs, const RGB& rhs)
+    {
+        RGB newColor {};
+
+        newColor.R = (lhs.R + rhs.R) / 2;
+        newColor.G = (lhs.G + rhs.G) / 2;
+        newColor.B = (lhs.B + rhs.B) / 2;
+
+        return newColor;
+    }
+};
+
 /**
- * @brief This namespace should not be used in the main function directly.
+ * @brief Color conversion functions.
  *
  */
-namespace pfs {
+namespace converter {
 
     /**
-     * @brief Validate whether the string is a valid hex code.
+     * @brief Converts string to hex code.
      *
-     * @param s The passed string
-     * @return true
-     * @return false
+     * @param str String to be converted
+     * @return    The hex code
      */
-    bool validate_hex_notation(const std::string& s)
+    int str_to_hex(const std::string str)
     {
-        return s.length() == 6
-            && std::all_of(s.begin(), s.end(), [](unsigned char c) {
-                   return std::isxdigit(c);
-               });
+        unsigned int x = 0;
+        std::stringstream ss;
+
+        ss << std::hex << str;
+        ss >> x;
+
+        return static_cast<int>(x);
+    }
+
+    /**
+     * @brief Converts hex code to string.
+     *
+     * @param hex The hex code
+     * @return    String
+     */
+    std::string hex_to_str(const unsigned int hex)
+    {
+        std::stringstream ss;
+
+        ss << std::hex << hex;
+        return ss.str();
+    }
+
+    /**
+     * @brief Converts hex to RGB.
+     *
+     * @param hex The hex code
+     * @return    RGB value
+     */
+    RGB hex_to_rgb(const unsigned int hex)
+    {
+        struct RGB rgbColor;
+
+        rgbColor.R = ((hex >> 16) & 0xFF) / 255.0;
+        rgbColor.G = ((hex >> 8) & 0xFF) / 255.0;
+        rgbColor.B = ((hex)&0xFF) / 255.0;
+
+        return rgbColor;
+    }
+
+    /**
+     * @brief Converts RGB to hex.
+     *
+     * @param rgb The RGB value
+     * @return    The hex code
+     */
+    int rgb_to_hex(const RGB& rgb)
+    {
+        int hexInt[] = {
+            static_cast<int>(rgb.R),
+            static_cast<int>(rgb.G),
+            static_cast<int>(rgb.B),
+        };
+
+        unsigned int hex = ((hexInt[0] & 0xff) << 16)
+            + ((hexInt[1] & 0xff) << 8) + (hexInt[2] & 0xff);
+        return static_cast<int>(hex);
     }
 }
 
 namespace ns {
 
+    /**
+     * @brief Get the colors from user.
+     *
+     * @param pColors The colors container to be filled
+     * @warning The argument will be overridden.
+     */
     void get_colors_from_user(Colors& pColors)
     {
         std::string input {};
@@ -70,10 +145,15 @@ namespace ns {
      *
      * @param colors The colors container
      */
-    void validate_hex(const Colors& pColors)
+    void validate_color(const Colors& pColors)
     {
         for (const auto& it : pColors) {
-            bool flag = pfs::validate_hex_notation(it);
+            // The length must be 6 letters with '0-9 a-f A-F' range.
+            bool flag = it.length() == 6
+                && std::all_of(it.begin(), it.end(), [](unsigned char c) {
+                       return std::isxdigit(c);
+                   });
+
             if (!flag) {
                 std::cerr << "error: Hex code " << it << " is invalid.\n";
                 break;
@@ -84,35 +164,23 @@ namespace ns {
     /**
      * @brief The color mixer's algorithm.
      *
-     * @param pColors The colors container
+     * @param pColors The colors container containing hex-code strings
      */
     void mix(const Colors& pColors)
     {
-        struct RGB {
-            double R;
-            double G;
-            double B;
-        };
+        RGB newColor {0.0, 0.0, 0.0};
 
-        std::stringstream ss {};
-        RGB newColor {};
+        for (const auto& it : pColors) {
+            int hex = converter::str_to_hex(it);
+            RGB rgb = converter::hex_to_rgb(hex);
 
-        for (size_t i = 0, len = pColors.size(); i < len; i++) {
-            RGB color {};
-            unsigned int value = 0U;
-
-            ss << std::hex << pColors.at(i);
-            ss >> value;
-
-            color.R = ((value >> 16) & 0xFF) / 255.0;
-            color.G = ((value >> 8) & 0xFF) / 255.0;
-            color.B = (value & 0xFF) / 255.0;
-
-            newColor = {(newColor.R + color.R) / 2, (newColor.G + color.G) / 2,
-                (newColor.B + color.B) / 2};
-
-            ss.clear();
+            newColor += rgb;
         }
+
+        int hex = converter::rgb_to_hex(newColor);
+        std::string str_newColor = converter::hex_to_str(hex);
+
+        std::cout << "The new color is: " << str_newColor << '\n';
     }
 } // namespace ns
 
@@ -121,7 +189,7 @@ int main()
     Colors colors {};
 
     ns::get_colors_from_user(colors);
-    ns::validate_hex(colors);
+    ns::validate_color(colors);
     ns::mix(colors);
 
     return EXIT_SUCCESS;
